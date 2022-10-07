361ab789d0feec542763e1885969b75f21742eab
Don Truong <dong23102003@gmail.com>
2022-10-07 19:33:17 -0400

    style: refactor code

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,12 @@
 <template>
-    <DynamicAvatar />
-    <SocialInformation />
-    <TypingText constantText="Hi, I'm " :dynamicTexts="roles" />
+    <IntroductionPane />
     <notifications class="notification" position="bottom right" />
 </template>
 
 <script setup lang="ts">
-import DynamicAvatar from './components/DynamicAvatar.vue';
-import TypingText from './components/TypingText.vue';
-import SocialInformation from './components/SocialInformation.vue';
 import { onMounted } from 'vue';
 import { notify } from '@kyvg/vue3-notification';
-
-const roles = [
-    'Don Truong',
-    'Đông Trương',
-    'from Vietnam',
-    'a sophomore at MSU',
-    'a former SWE Intern at Holistics',
-    'a former SWE Intern at OED',
-    "a former SWE Intern at Chang's Kitchen",
-    'a dog person :)',
-];
+import IntroductionPane from './components/introduction/IntroductionPane.vue';
 
 onMounted(() => {
     notify({
---
+++ src/components/introduction/IntroductionPane.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <DynamicAvatar />
+        <SocialInformation />
+        <TypingText constantText="Hi, I'm " :dynamicTexts="roles" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import DynamicAvatar from './DynamicAvatar.vue';
+import TypingText from './TypingText.vue';
+import SocialInformation from './SocialInformation.vue';
+
+const roles = [
+    'Don Truong',
+    'Đông Trương',
+    'from Vietnam',
+    'a sophomore at MSU',
+    'a former SWE Intern at Holistics',
+    'a former SWE Intern at OED',
+    "a former SWE Intern at Chang's Kitchen",
+    'a dog person :)',
+];
+</script>
+
+<style lang="scss">
+.notification {
+	.notification-title {
+		font-size: 15px;
+		font-weight: bolder;
+	}
+	.notification-content {
+		font-size: 15px;
+	}
+}
+</style>
