a4689f718cbeeeff292a9d6ba4855ab2575e6280
Don Truong <dong23102003@gmail.com>
2022-11-05 20:09:53 -0400

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ set -e
 npm run build
 # navigate into the build output directory
 cd dist
+cp index.html 404.html
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
