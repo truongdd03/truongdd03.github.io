960372b8f1189911242189bad68d97b34c432c72
Don Truong <dong23102003@gmail.com>
2022-11-27 23:42:56 -0500

    add function to get spotify activity

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -1,12 +1,62 @@
-import SpotifyWebApi from 'spotify-web-api-node';
+import querystring from 'querystring';
 
-var spotifyApi = new SpotifyWebApi({
-    clientId: 'fcecfc72172e4cd267473117a17cbd4d',
-    clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
-    redirectUri: 'http://www.example.com/callback'
-});
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
+const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+const refresh_token = import.meta.env.VITE_SPOTIFY_REFRESH_TOKEN;
+
+const getAccessToken = async () => {
+    const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+    const response = await fetch(TOKEN_ENDPOINT, {
+        method: "POST",
+        headers: {
+            Authorization: `Basic ${basic}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: querystring.stringify({
+            grant_type: "refresh_token",
+            refresh_token,
+        }),
+    });
+
+    return response.json();
+};
+
+const getNowPlaying = async () => {
+    const { access_token } = await getAccessToken();
+    return fetch(NOW_PLAYING_ENDPOINT, {
+        headers: {
+            Authorization: `Bearer ${access_token}`,
+        },
+    });
+};
+
+const getNowPlayingItem = async () => {
+    const response = await getNowPlaying();
+    if (response.status === 204 || response.status > 400) {
+        return false;
+    }
+    try {
+        const song = await response.json();
+        const albumImageUrl = song.item.album.images[0].url;
+        const artist = song.item.artists.map((artist: any) => artist.name).join(", ");
+        const isPlaying = song.is_playing;
+        const songUrl = song.item.external_urls.spotify;
+        const title = song.item.name;
+    
+        return {
+            albumImageUrl,
+            artist,
+            isPlaying,
+            songUrl,
+            title,
+        };
+    } catch (err: any) {
+        return false;
+    }
+}
 
 (async () => {
-    spotifyApi.setAccessToken(import.meta.env.VITE_SPOTIFY_API_KEY);
-    console.log(await spotifyApi.getMyCurrentPlayingTrack());
+    console.log((await getNowPlayingItem()));
 })();
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import { fileURLToPath, URL } from 'node:url';
-
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vueJsx()],
+    plugins: [
+        vue(),
+        vueJsx(),
+        NodeGlobalsPolyfillPlugin({
+            buffer: true
+        })
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
+            web3: path.resolve(__dirname, "./node_modules/web3/dist/web3.min.js"),
         },
     }
 });
