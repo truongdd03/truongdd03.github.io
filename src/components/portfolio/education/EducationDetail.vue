f856e1fcf050946074d5b93bdb69a92437fa83b6
Don Truong <dong23102003@gmail.com>
2024-01-13 12:44:56 -0500

    fix lint

--- src/components/portfolio/education/EducationDetail.vue
+++ src/components/portfolio/education/EducationDetail.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="border-3 surface-border border-round p-3 h-full">
         <div class="flex align-items-center justify-content-center ">
-            <img :src="education?.icon" style="height: 40px;" />
-            <p class="text-2xl ml-2 text-center">{{ education?.school }}</p>
+            <img :src="education?.icon" style="height: 40px;" alt="School" />
+            <p class="text-2xl ml-2 text-center">
+                {{ education?.school }}
+            </p>
         </div>
         <div class="flex mt-3 secondary-text text-color-secondary">
             <p>{{ education?.degree }}</p>
-            <p class="ml-auto text-right">{{ education?.duration }}</p>
+            <p class="ml-auto text-right">
+                {{ education?.duration }}
+            </p>
         </div>
     </div>
 </template>
